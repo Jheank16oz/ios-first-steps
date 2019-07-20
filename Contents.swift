c6e608e475115b8ddb9343ac8b517d667914fd46
Jhean <jheank16@gmail.com>
2019-07-20 16:01:11 -0500

    Vars and constants

--- Contents.swift
+++ Contents.swift
@@ -1,8 +1,12 @@
 import UIKit
 
+// MARK: Variables y Constantes
+
 // se declara una variable de un millon
 var totalAccount:Float = 1000000
-
 // tambien se pueden delimidar las unidades con _
 totalAccount = 1_000_000
 
+// constantes
+let name:String =  "Jhean Carlos"
+let lastName:String =  "Piñeros"
